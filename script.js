73a98709e49c643be4ba68ae19e9926d92ce18bf
caroexe <100613743+caroexe@users.noreply.github.com>
2024-09-18 13:08:46 +0200

    modify  playRound and playGame

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const btns = Array.from(document.querySelectorAll('#buttons'));
+const btns = document.querySelector('#buttons');
 const message = document.querySelector('#message');
 const scorePlayer = document.querySelector('#score-player');
 const scoreComputer = document.querySelector('#score-computer');
@@ -8,13 +8,25 @@ const recordComputer = document.querySelector('#record-computer');
 let playerScore = 0;
 let computerScore = 0;
 
-btns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        if (playerScore >= 5 || computerScore >= 5) {
-            return;
-        }
-        
-    })
+btns.addEventListener('click', (event) => {
+    let target = event.target;
+    let choice = '';
+
+    switch(target.id) {
+        case 'rockBtn':
+            choice = 'Rock';
+            break;
+        case 'paperBtn':
+            choice = 'Paper';
+            break;
+        case 'scissorsBtn':
+            choice = 'Scissors';
+            break;
+        default:
+            break;        
+    }
+
+    playGame(choice);
 });
 
 function getComputerSelection () {
@@ -23,13 +35,13 @@ function getComputerSelection () {
 
     switch (computerNumber) {
         case 1:
-            computerGuess = 'rock';
+            computerGuess = 'Rock';
             break;
         case 2:
-            computerGuess = 'paper';
+            computerGuess = 'Paper';
             break;
         case 3:
-            computerGuess = 'scissors';
+            computerGuess = 'Scissors';
             break;
         default:
             console.log('Error has occured trying to generate random Computer selection');
@@ -48,30 +60,24 @@ function playRound (playerChoice, computerChoice) {
     if (playerChoice === 'Rock') {
         if (computerChoice === 'Paper') {
             result = `You Lose! ${computerChoice} beats ${playerChoice}`;
-            computerScore += 1;
         }  else if (computerChoice === 'Scissors'){
             result = `You Win! ${playerChoice} beats ${computerChoice}`;
-            playerScore += 1;
         } else {
             result = `It's a tie!`;
         }
     } else if (playerChoice === 'Paper') {
         if (computerChoice === 'Scissors') {
             result = `You Lose! ${computerChoice} beats ${playerChoice}`;
-            computerScore += 1;
         } else if (computerChoice === 'Rock'){
             result = `You Win! ${playerChoice} beats ${computerChoice}`;
-            playerScore += 1;
         } else {
             result = `It's a tie!`;
         }
     } else if (playerChoice === 'Scissors') {
         if (computerChoice === 'Rock') {
             result = `You Lose! ${computerChoice} beats ${playerChoice}`;
-            computerScore += 1;
         } else if (computerChoice === 'Paper'){
             result = `You Win! ${playerChoice} beats ${computerChoice}`;
-            playerScore += 1;
         } else {
             result = `It's a tie!`;
         }
@@ -80,6 +86,40 @@ function playRound (playerChoice, computerChoice) {
     return result;
 }
 
+function playGame (playerSelect) {
+    let playerSelection = playerSelect;
+    let computerSelection = getComputerSelection();
+
+    let roundResult = playRound(playerSelection,computerSelection);
+
+    if (roundResult.search('You Win!') > -1) {
+        console.log('WIN');
+        playerScore++;
+    } else if (roundResult.search ('You Lose!') > -1 ) {
+        console.log('LOOSE');
+        computerScore++;
+    } 
+
+    scorePlayer.textContent = playerScore;
+    scoreComputer.textContent = computerScore;
+    message.textContent = roundResult;
+    recordPlayer.appendChild(createParagWithText(playerSelection));
+    recordComputer.appendChild(createParagWithText(computerSelection));    
+
+    if (playerScore >= 5 && computerScore < 5) {
+        message.textContent = 'Game Over. You Win!';
+      } else if (playerScore < 5 && computerScore >= 5) {
+        message.textContent = 'Game Over. You Lose!';
+      }
+
+}
+
+function createParagWithText (text) {
+    const p = document.createElement('p');
+    p.textContent = text;
+
+    return p;
+}
 
 function random (number) {
     return Math.floor(Math.random() * number + 1);
